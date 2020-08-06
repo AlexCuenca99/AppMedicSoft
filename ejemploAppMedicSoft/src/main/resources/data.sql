596de3d5ed66c7e358c033b69d7a3b5405c712cd
SYSTEMarket <SYSTEMarket@PegasoMc-AV>
2020-08-06 13:51:46 -0500

    data

--- ejemploAppMedicSoft/src/main/resources/data.sql
+++ ejemploAppMedicSoft/src/main/resources/data.sql
@@ -1,5 +1,3 @@
 INSERT INTO pacientes(ci, apellido, correo, edad, foto, historia_clinica, nombre, numero_telefonico)
 VALUES('1718276551','Cuenca','Alex@','20','56157028a68c43c2bf975686851eec65','1234','Alex','0989181061');
 
-INSERT INTO equipo_medico(area_uso, cantidad, codigo, fecha_comp, nombre, precio)
-VALUES('Fisioterapia','20','6577F','20 Octubre','Electrodos','12343');
